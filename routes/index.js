735fd5c13297a3c4de27799bd5370bc862ffd708
gregorio67 <kkimdoy@gmail.com>
2020-03-13 10:21:36 +0900

    :

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
+/** npm install express **/
 const express = require('express');
+/** npm install request and request-promise */
 const Request = require('request-promise');
 
 const router = express.Router();
 
-const URI = 'http://127.0.0.1:7070/userTask.do'
+const URI = 'http://192.168.219.108:7070/userTask.do'
 router.post('/slack.do', (request, response) => {
     console.log(request.body)
     console.log(request.body.time)
     console.log(request.body.offset)
     console.log(request.body.pageSize)
 
+    /** External service stack **/
     var options = {
         method: 'POST',
         uri: URI,
@@ -29,5 +32,10 @@ router.post('/slack.do', (request, response) => {
             console.log(err);
         })
 })
+
+router.get('/', (request, response) => {
+    var message = 'Welcome to wire webhook !!!'
+    response.status(200).send(message);
+});
 
 module.exports = router;
